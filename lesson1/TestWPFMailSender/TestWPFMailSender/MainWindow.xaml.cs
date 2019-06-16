d5a0b21e64ad0ea2cb8a97565f0b0bf8ac5a0fac
alebastr12 <thegrace12@gmail.com>
2019-06-17 00:02:23 +0300

    Проект примитивного рассыльщика почты

---
+++ lesson1/TestWPFMailSender/TestWPFMailSender/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+
+
+namespace TestWPFMailSender
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        EmailSendServiceClass Email;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Email = new EmailSendServiceClass();
+            DataContext = Email;
+            
+        }
+
+        private void SendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            string[] EmailsTo = EmailToBox.Text.Split(';');
+            if (EmailsTo.Length > 0)
+            {
+                Email.To.Clear();
+                foreach(string email in EmailsTo)
+                {
+                    Email.To.Add(new Person(email,"Имя"));
+                }
+                Email.Password = PasswordBox.SecurePassword;
+                //Email.EmailServer.Port = Convert.ToInt32(PortBox.Text);
+                try
+                {
+                    Email.SendMessage(SubjectBox.Text, BodyBox.Text);
+                    MessageBox.Show("Почта отправлена успешно!", "Успех!!!",
+                            MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при отправке почты \r\n{ex.Message}", "Ошибка!",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
---
+++ lesson1/TestWPFMailSender/TestWPFMailSender/Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWPFMailSender
+{
+    class Server
+    {
+        public string ServerName { get; set; }
+        public int Port { get; set; }
+        public override string ToString()
+        {
+            return ServerName + $":{Port}";
+        }
+    }
+}
